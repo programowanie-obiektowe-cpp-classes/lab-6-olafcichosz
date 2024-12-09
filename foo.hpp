1fcf25146a7a28cb65566e9cd0d92d75dd1ada6b
Olaf Cichosz <01171645@pw.edu.pl>
2024-12-09 14:17:23 +0100

    zadanie!

--- foo.hpp
+++ foo.hpp
@@ -1,16 +1,15 @@
-#pragma once
-
 #include "Human.hpp"
-
 #include <algorithm>
 #include <list>
 #include <vector>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    std::vector< char > retval(people.size());
-
-    // Twoja implementacja tutaj
+    std::vector< char > retval(static_cast< std::vector< char >::size_type >(people.size()), ' ');
+    std::for_each(people.begin(), people.end(), [](Human& person) { person.birthday(); });
+    std::transform(people.rbegin(), people.rend(), retval.begin(), [](const Human& person) -> char {
+        return person.isMonster() ? 'n' : 'y';
+    });
 
     return retval;
 }
